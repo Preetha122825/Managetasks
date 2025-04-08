9e63d7a0a79fb1e55d4bda15db3772f0b604f8db
Preetha122825 <preethacse25@gmail.com>
2025-04-08 12:40:50 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskComponent } from './task/task.component';
+import { ViewresultComponent } from './viewresult/viewresult.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,TaskComponent],
+  imports: [RouterOutlet,TaskComponent,ViewresultComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
